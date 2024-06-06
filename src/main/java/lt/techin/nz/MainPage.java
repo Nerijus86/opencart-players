4649a61b406ca37cf4fba6eaee2f1e949e76092e
Nerijus <nerijus.zaveckas@gmail.com>
2024-06-07 00:12:02 +0300

    Problems with wait

--- src/main/java/lt/techin/nz/MainPage.java
+++ src/main/java/lt/techin/nz/MainPage.java
@@ -4,8 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MainPage extends BasePage{
+import java.time.Duration;
+
+public class MainPage extends BasePage {
     public MainPage(WebDriver driver) {
         super(driver);
     }
@@ -26,7 +31,10 @@ public class MainPage extends BasePage{
     }
 
     public void setButtonShowItemsAsLis() {
+
         new Actions(driver).scrollToElement(buttonShowItemsAsLis).perform();
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.elementToBeClickable(buttonShowItemsAsLis));
         buttonShowItemsAsLis.click();
     }
 
--- src/test/java/lt/techin/nz/UITest.java
+++ src/test/java/lt/techin/nz/UITest.java
@@ -1,19 +1,30 @@
 package lt.techin.nz;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.time.Duration;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.By;
 
 public class UITest extends BaseTest {
 
 
     @Test
-    void firstTest() throws InterruptedException {
+    void firstTest() {
         MainPage mainPage = new MainPage(driver);
-
         mainPage.setButtonMP3Players();
         mainPage.setButtonShowAllMP3Players();
         mainPage.setButtonShowItemsAsLis();
-        Thread.sleep(Duration.ofSeconds(5));//ištrinti
+
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/test-data.csv")
+    void itemsCheck(String itemName) {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.setButtonMP3Players();
+        mainPage.setButtonShowAllMP3Players();
+        mainPage.setButtonShowItemsAsLis();
+        Assertions.assertTrue(driver.findElement(By.linkText(itemName)).isDisplayed());
     }
 }
